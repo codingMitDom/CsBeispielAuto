8f6f355782d198cbe1c11816695500525df0e5b5
dwit.at <137614685+codingMitDom@users.noreply.github.com>
2024-07-19 10:10:01 +0200

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial
+{
+    internal class Auto
+    {
+        // Felder
+        public string farbe;
+        public int hoechstGeschwindigkeit;
+
+
+        // Methoden
+        // Gibt die Farbe, die Höchstgeschwindigkeit und eine Text auf der Konsole aus.
+        public void Fahren()
+        {
+            Console.WriteLine($"Das Auto hat die Farbe {farbe}");
+            Console.WriteLine($"Das Auto hat die Höchstgeschwindigkeit {hoechstGeschwindigkeit}");
+            Console.WriteLine($"Das Auto fährt...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CsTutorial;
+
+Auto ferrari = new Auto();
+ferrari.farbe = "rot";
+ferrari.hoechstGeschwindigkeit = 240;
+
+
+Auto audi = new Auto();
+audi.farbe = "blau";
+audi.hoechstGeschwindigkeit = 200;
+
+ferrari.Fahren();
+audi.Fahren();
