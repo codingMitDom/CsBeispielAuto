e23a086e516c286f74fe4a6c544b20e61d592649
dwit.at <137614685+codingMitDom@users.noreply.github.com>
2024-07-19 10:32:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,10 +4,4 @@ Auto ferrari = new Auto();
 ferrari.farbe = "rot";
 ferrari.hoechstGeschwindigkeit = 240;
 
-
-Auto audi = new Auto();
-audi.farbe = "blau";
-audi.hoechstGeschwindigkeit = 200;
-
 ferrari.Fahren();
-audi.Fahren();
